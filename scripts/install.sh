c2caf9976e51b6f11467a9109442e9360cc3be08
Alexander Efimov <efimokthefatass@gmail.com>
2022-06-08 09:45:14 +0300

    leave version in file name

--- scripts/install.sh
+++ scripts/install.sh
@@ -5,8 +5,7 @@ npm i
 npm run build
 
 # MAIN APP AUTOSTART
-mv dist/tote-ui* dist/tote-ui.AppImage
-chmod +x dist/tote-ui.AppImage
+chmod +x dist/tote-ui*.AppImage
 mkdir ~/.inenergy
 echo '{ "CRITICAL_CONCENTRATION": 75 }' > ~/.inenergy/config.json
-echo "~/inenergy-gui/tote-ui/dist/tote-ui.AppImage > ~/.inenergy/tote-ui.log"
+echo "~/inenergy-gui/tote-ui/dist/tote-ui*.AppImage > ~/.inenergy/tote-ui.log"
