1648fc63d8d30ce0d5cfc07a76a55515c30034cf
Alexander Efimov <efimokthefatass@gmail.com>
2022-06-23 15:54:16 +0300

    update instructions

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+cd "$( dirname "$0" )/.."
 
 # MAIN APP INSTALLATION
 npm i
 npm run build
 
 # MAIN APP AUTOSTART
-chmod +x dist/tote-ui*.AppImage
+chmod +x dist/hydrogen-energy-ui*.AppImage
+mkdir -p ~/inenergy-gui/dist
+cp dist/hydrogen-energy-ui*.AppImage ~/inenergy-gui/dist/
 mkdir ~/.inenergy
-echo '{ "CRITICAL_CONCENTRATION": 75 }' > ~/.inenergy/config.json
-echo "~/inenergy-gui/tote-ui/dist/tote-ui*.AppImage > ~/.inenergy/tote-ui.log"
+echo '~/inengergy-gui/dist/hydrogen-energy-ui*.AppImage > ~/.inenergy/hydrogen-energy-ui.log' > ~/.config/openbox/autostart
+echo '{"CRITICAL_CONCENTRATION": 75}' > ~/.inenergy/config.json
